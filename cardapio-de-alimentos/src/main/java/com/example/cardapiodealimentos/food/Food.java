6953a92f55555146b870b0a114ba812e570cc310
pedromos16 <pedro.marcio.os7@gmail.com>
2023-11-19 19:29:41 -0300

    consigo exibir os dados do banco, faltava o @Data

--- cardapio-de-alimentos/src/main/java/com/example/cardapiodealimentos/food/Food.java
+++ cardapio-de-alimentos/src/main/java/com/example/cardapiodealimentos/food/Food.java
@@ -1,13 +1,17 @@
 package com.example.cardapiodealimentos.food;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Table(name = "foods")
 @Entity(name = "foods")
 public class Food {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
+    private String title;
     private String image;
     private Integer price;
+
+    //ou use o @Data que gera getter e setter ou coloque manualmnete se não os dados do banco vem vazios
 }
