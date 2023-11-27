c05df9bb88810457383ad84fa403cef0c84dfc11
pedromos16 <pedro.marcio.os7@gmail.com>
2023-11-27 00:16:49 -0300

    Inicio do front

---
+++ front-cardapio/src/componets/card/card.tsx
@@ -0,0 +1,16 @@
+interface CardProps {
+    price: number,
+    title: string,
+    image: string
+}
+
+export function Card({ price, image, title} : CardProps) {
+  return (
+    <div className='card'>
+      <img>
+        <h2></h2>
+        <p><b>Valor:</b></p>
+      </img>
+    </div>
+  )
+}
