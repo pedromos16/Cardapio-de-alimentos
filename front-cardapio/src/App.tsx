74593e9bee0ce70201c3d4252a20e6f11aaa38ec
pedromos16 <pedro.marcio.os7@gmail.com>
2023-12-04 21:40:46 -0300

    exibindo dados, falta a insercao

--- front-cardapio/src/App.tsx
+++ front-cardapio/src/App.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' 
+import { useFoodData } from './hooks/useFoodData'
+import { Card } from './components/card/card'
 
 function App() {
-  
+  const {data} = useFoodData();
 
   return (
     <div className='container'>
       <h1>Cardápio</h1>
       <div className='card-grid'>
-        
+        {data?.map(foodData => 
+          <Card key={foodData.id}
+            price = {foodData.price} 
+            title = {foodData.title} 
+            image = {foodData.image}
+          />
+        )}
       </div>
     </div>
   )
